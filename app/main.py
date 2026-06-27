cc67cda7abdc78c1e164a38a4d946a5a67df7168
Navaneeth <navaneethsnp26@gmail.com>
2026-06-27 14:12:34 +0530

    git test

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
 from routers import company,job
+from database import Base,engine
 
 app = FastAPI()
+print("engine is :",engine)
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(company.router)
 
