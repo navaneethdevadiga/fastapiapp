f1386697002eb78a6bd2f618c246dc7d11301d5d
Navaneeth <navaneethsnp26@gmail.com>
2026-06-30 15:36:20 +0530

    git test

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,19 +1,49 @@
-import Welcome from "./components/Welcome";
 import NavBar from "./components/NavBar";
+import Welcome from "./components/welcome";
+import Footer from "./components/Footer";
 import CompanyCard from "./components/CompanyCard";
 import JobCard from "./components/JobCard";
-import Footer from "./components/Footer";
-function App(){
-  return(
-    <>
-    <NavBar />
-    <Welcome />
-    <br />
-    <CompanyCard />
-    <JobCard />
-    <Footer />
-    </>
-  )  
-}
+import {useState,useEffect} from "react";
+import { getCompanies } from "./Services/CompanyService";
+import type { Company } from "./types/company";
+function App() {
+  const[loading,setLoading]=useState(true);
+  const[error,setError]=useState<Error|null>(null);
+  const[companies,setCompanies]=useState<Company[]>([]);
+  async function fetchCompanies(){
+    setLoading(true);
+    try{
+      const companies=await getCompanies();
+      setCompanies(companies);
+    } catch (err) {
+      setError(err as Error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
-export default App
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+
+  if(loading){
+    return <div>Loading...</div>;
+  }
+
+  if(error){
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <>
+      <NavBar />
+      <Welcome />
+      <CompanyCard
+      companies={companies}/>
+      <JobCard />
+      <Footer />
+    </>
+  )
+}
+  
+export default App;
--- schemas/company.py
+++ schemas/company.py
@@ -4,20 +4,19 @@ from .job import JobResponse
 
 
 class CompanyBase(BaseModel):
-    name: str
-    email: str
-    phone: str
-    location: str
-
-class CompanyCreate(CompanyBase):
-    pass
-
-class CompanyUpdate(CompanyBase):
     name: Optional[str] = None
     email: Optional[str] = None
     phone: Optional[str] = None
     location: Optional[str] = None
 
+class CompanyCreate(CompanyBase):
+    name: str
+    email: str
+    phone: str
+    location: str
+
+class CompanyUpdate(CompanyBase):
+    pass
 class CompanyResponse(CompanyBase):
     id:int
     jobs: list[JobResponse]
