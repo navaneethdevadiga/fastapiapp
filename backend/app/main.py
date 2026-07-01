7338289baab8d79cd9a759959a422c02aa518c6d
Navaneeth <navaneethsnp26@gmail.com>
2026-07-01 16:55:54 +0530

    git test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from routers import company,job
+from routers import company,job,auth
 from database import Base,engine
-from models import job as job_model,company as company_model
+from models import job as job_model,company as company_model,users as user_model
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -14,7 +14,7 @@ app.add_middleware(
 )
 
 # Base.metadata.create_all(bind=engine)
-
+app.include_router(auth.router)
 app.include_router(company.router)
 app.include_router(job.router)
 
---
+++ backend/utils/security.py
@@ -0,0 +1,12 @@
+from pwdlib import PasswordHash
+
+pwd_context = PasswordHash.recommended()
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(password: str, hashed_password: str):
+    return pwd_context.verify(password, hashed_password)
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
